ad437e053813ca754f3e4f90baed2c015ae85a3a
Luiz Carlos Vieira <luigivieira@gmail.com>
2016-03-04 12:44:14 -0300

    Another attempt to fix the scrollarea in the about dialog

--- src/aboutwindow.cpp
+++ src/aboutwindow.cpp
@@ -29,13 +29,7 @@ ct::AboutWindow::AboutWindow(QWidget *pParent) :
 	ui(new Ui::AboutWindow)
 {
     ui->setupUi(this);
-
-	QString sText = "<html><head/><body><p><span style=\" font-weight:600;\">CamTest - A simple webcam application (version %1)</span></p><p align=\"justify\">Copyright (C) 2016 <a href=\"http://www.luiz.vieira.nom.br/\"><span style=\" text-decoration: underline; color:#0000ff;\">Luiz Carlos Vieira</span></a></p><p align=\"justify\">This is a simple webcam application mainly used to test the installation and building of Qt and OpenCV both in Windows and Linux - but I suppose it also works as a testing tool for webcams. :)</p><p align=\"justify\">The application icons are either from or based on the Oxygen Icons Set, downloaded as <a href=\"https://github.com/pasnox/oxygen-icons-png\"><span style=\" text-decoration: underline; color:#0000ff;\">PNGs from Felipe Azevedo (pasnox)</span></a> and <a href=\"https://techbase.kde.org/Projects/Oxygen/Licensing\"><span style=\" text-decoration: underline; color:#0000ff;\">licensed under LGPL from KDE</span></a>.</p><p align=\"justify\">LICENSE</p><p align=\"justify\">This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.</p><p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.</p><p>You should have received a copy of the GNU General Public License along with this program. If not, see <a href=\"http://www.gnu.org/licenses/\"><span style=\" text-decoration: underline; color:#0000ff;\">http://www.gnu.org/licenses/</span></a>.</p></body></html>";
-	QLabel *pLabel = new QLabel(this);
-	pLabel->setText(sText.arg(CT_VERSION));
-	pLabel->setWordWrap(true);
-	pLabel->setOpenExternalLinks(true);
-	ui->scrollArea->setWidget(pLabel);
+	ui->titleLabel->setText(ui->titleLabel->text().arg(CT_VERSION));
 }
 
 // +-----------------------------------------------------------
